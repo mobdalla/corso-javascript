4d4153ebfd19f5b7692b2356a1ce63a0ecbb23be
mobdalla <76433493+mobdalla@users.noreply.github.com>
2024-05-27 17:46:53 +0200

    esericizi javascript

---
+++ js-02-variables/tell-myfortune/01-tell-myfortune.js
@@ -0,0 +1,6 @@
+let numOfChildren =3 ;
+let partnerName="ancora";
+let JobTitle="Software Develop";
+let GeoLocation;
+
+console.log('You will be a '+JobTitle+' in 2025, and married to '+partnerName+' with '+numOfChildren+' kids')
