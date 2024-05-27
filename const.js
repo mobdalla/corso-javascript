d3efd476cbc609fec09a1d5430d33acc1682a65e
mobdalla <76433493+mobdalla@users.noreply.github.com>
2024-05-27 17:17:58 +0200

    second file js

---
+++ const.js
@@ -0,0 +1,8 @@
+let x = 88;
+const y = 77;
+
+x = 9;
+console.log('x = ',x)
+//y = 17; // TypeError: assignment to constant variable
+console.log('y = ',y)
+const y =55; //SyntaxError: const cannot be reassignment
