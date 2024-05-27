61b642aab60829f4ecf1217ba2a80cea54dc11a6
mobdalla <76433493+mobdalla@users.noreply.github.com>
2024-05-27 17:10:56 +0200

    file js con let

---
+++ let.js
@@ -0,0 +1,11 @@
+let x = 88;
+let t;
+console.log('value of x',x);
+for (let i = 0; i < 10; i++) {
+    t = i;
+    console.log('inside i = ', i);
+    console.log('inside t = ', t);
+    
+}
+console.log('outside i=', i)
+console.log('outside t =',t)
